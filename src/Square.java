b0a6304a4d1f8817d4e40385eb4a8ad318f53f40
Brady <brady.drew.logan@gmail.com>
2021-05-20 10:30:33 -0500

    can now determine if an integer is a square

--- src/Square.java
+++ src/Square.java
@@ -1,5 +1,14 @@
 public class Square {
 	public static boolean isSquare(int n) {
+		if(Math.sqrt(n) % 1 == 0) {
+			return true;
+		}
 		return false;
+	}
+	
+	public static void main(String args[])
+	{
+		System.out.print(Square.isSquare(4));
+		
 	}
 }
